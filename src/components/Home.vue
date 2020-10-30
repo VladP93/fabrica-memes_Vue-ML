b99ce7cd27dd530452d860940349c74efdde845c
Vladimir Paniagua <vladimirpaniagua@gmail.com>
2020-10-30 03:13:17 -0600

    Main page/Details page finished

---
+++ src/components/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <v-container>
+    <v-layout xs12 pl-2>
+      <v-flex>
+        <input type="file" ref="fileInput" id="file" @change="processFile($event)" />
+      </v-flex>
+    </v-layout>
+    <br>
+    <v-layout xs12 pb-3>
+      <v-flex>
+        <v-btn color="primary" @click="uploadFile">Enviar</v-btn>
+      </v-flex>
+    </v-layout>
+    <v-divider>
+    </v-divider>
+      <v-layout row wrap align-center>
+        <v-flex xs12>
+          <v-container fluid class="pl-0 pr-0">
+            <v-layout row wrap>
+              <v-flex xs12 sm6 lg3 v-for="image in images" v-bind:key="image.id">
+                <v-card>
+                  <v-img
+                  class="white--text"
+                    height="200px"
+                    :src="image.url">
+                  </v-img>
+                  <v-card-title>
+                    <div>
+                      <span class="grey--text">
+                        {{image.name}}
+                      </span>
+                      <v-chip>
+                        {{image.scorePromedio}}
+                      </v-chip>
+                      <br>
+                    </div>
+                  </v-card-title>
+                  <v-card-actions>
+                    <v-btn flat @click="goToImageDetail(image.id)">Explore</v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
+      </v-layout>
+  </v-container>
+</template>
+
+<script>
+import {firestore} from '../main'
+import {storage} from '../main'
+
+  export default {
+    name: 'Home',
+    mounted:function(){
+      console.log(this.images)
+    },
+    data: () => ({
+      file:'',
+      images:[],
+    }),
+    methods:{
+      processFile: function(event){
+        this.file = event.target.files[0]
+      },
+      uploadFile: function(){
+        let name = `${+new Date()}-${this.file.name}`
+        let metadata = {contentType: this.file.type}
+        storage.child(name).put(this.file, metadata).then(res =>res.ref.getDownloadURL()).then(url =>{
+          let image = {
+            url,
+            name,
+            createdAt:(+new Date()),
+            scorePromedio:0
+          }
+          firestore.collection('images').add(image)
+        })
+      },
+      goToImageDetail: function(id){
+        this.$router.push({path: `/image/${id}`})
+      }
+    },
+    firestore(){
+      return{
+        images: firestore.collection('images')
+      }
+    },
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
-import firebase from 'firebase';
-import VueFire from 'vuefire';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/storage';
+import {firestorePlugin} from 'vuefire';
 import router from './routes/index';
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyD--Hk_SDSTJVT3TDayhwEJ-mHdpltxSYU",
+  authDomain: "fabrica-memes-20a98.firebaseapp.com",
+  databaseURL: "https://fabrica-memes-20a98.firebaseio.com",
+  projectId: "fabrica-memes-20a98",
+  storageBucket: "fabrica-memes-20a98.appspot.com",
+  messagingSenderId: "593381490736",
+  appId: "1:593381490736:web:35494fcec316f01debadf5"
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firestore = firebaseApp.firestore();
+export const storage = firebaseApp.storage().ref('/images');
+
+Vue.use(firestorePlugin)
+
 new Vue({
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  router,
+  components: {App}
 }).$mount('#app')
